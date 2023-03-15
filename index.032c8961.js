42b9541d1cb7a11c904fee6da7a8144d788b938c
SzymonCierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-15 18:17:39 +0000

    Deploying to gh-pages from @ SzymonCierpik/goit-js-hw-10@5a74426fdc3b44379c6ad15866dfce1b768d4e5d 🚀

---
+++ index.032c8961.js
@@ -0,0 +1,2 @@
+console.log(fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages").then((function(t){if(!t.ok)throw new Error(t.status);return t.json()})));
+//# sourceMappingURL=index.032c8961.js.map
