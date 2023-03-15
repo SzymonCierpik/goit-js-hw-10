22807bcbf9deb8a67913f2daa3ca34ec0d2b8fde
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-15 16:22:37 +0100

    1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import './css/styles.css';
 import './fetchCountries.js';
 import debounce from 'lodash.debounce';
-
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('#country-list');
 const countryInfo = document.querySelector('#country-info');
 
-const countryHtml = `
-          <h2>${country.name}</h2>
+const countryHtml = `<h2>${country.name}</h2>
           <img src="${country.flag}" alt="${country.name} flag" />
           <p>Capital: ${country.capital}</p>
           <p>Population: ${country.population}</p>
-          <p>Languages: ${country.languages}</p>
-        `;
+          <p>Languages: ${country.languages}</p>`;
+
 countryInfo.innerHTML = countryHtml;
