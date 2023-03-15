5a74426fdc3b44379c6ad15866dfce1b768d4e5d
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-15 19:17:08 +0100

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,10 @@
 export function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages`;
-
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Problem-1');
-      }
-      return response.json();
-    })
-    .then(countries => {
-      return countries;
-    })
-    .catch(error => {
-      console.error('Problem-2:', error);
-    });
+  return fetch(
+    'https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages'
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
