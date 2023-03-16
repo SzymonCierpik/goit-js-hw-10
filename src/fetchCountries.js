56da546356dc523ddefc1f10e83920d3cd4cef1f
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-16 08:32:34 +0100

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -8,3 +8,17 @@ export function fetchCountries(name) {
     return response.json();
   });
 }
+
+
+function fetchCountries() {
+  const countryName =
+    'https://restcountries.com/v3.1/name/' +
+    countryInput.value.trim() +
+    '?fields=name,capital,population,flags,languages';
+  return fetch(`${countryName}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('#country-list');
-const countryInfo = document.querySelector('#country-info');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const searchCountry = debounce(() => {
   const searchTerm = searchBox.value.trim();
